b08cd5196d2a1db50414bd4f291f4d7797c088fb
George Spatacean <george.spatacean@gmail.com>
2021-04-17 15:04:40 +0300

    Add support for Creature API -  '/data/wow/creature/{creatureId}`

--- driver-app/src/main/java/com/gspatace/blizzard/driver/app/App.java
+++ driver-app/src/main/java/com/gspatace/blizzard/driver/app/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
@@ -51,6 +52,13 @@ public class App implements CommandLineRunner {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        String at = oAuthRestTemplate().getAccessToken().getValue();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.gspatace.blizzard");
+        context.refresh();
+        CreatureAPIDriver creatureAPIDriver = context.getBean(CreatureAPIDriver.class);
+        creatureAPIDriver.doTest(at);
     }
 }
 
