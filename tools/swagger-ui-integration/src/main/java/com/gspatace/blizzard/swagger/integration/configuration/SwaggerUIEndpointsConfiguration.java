783551336d3216b6ddd26951a72fb1f271fd770d
George Spatacean <george.spatacean@gmail.com>
2021-04-20 17:39:12 +0300

    Style/cleanup

--- tools/swagger-ui-integration/src/main/java/com/gspatace/blizzard/swagger/integration/configuration/SwaggerUIEndpointsConfiguration.java
+++ tools/swagger-ui-integration/src/main/java/com/gspatace/blizzard/swagger/integration/configuration/SwaggerUIEndpointsConfiguration.java
@@ -5,8 +5,6 @@ import com.gspatace.blizzard.swagger.integration.services.ApiDiscoveryService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.web.client.RestTemplate;
-import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
@@ -24,7 +22,7 @@ public class SwaggerUIEndpointsConfiguration {
         return () -> {
             final List<SwaggerResource> resources = new ArrayList<>();
 
-            apis.stream().forEach( res -> {
+            apis.stream().forEach(res -> {
                 final SwaggerResource swaggerResource = new SwaggerResource();
                 swaggerResource.setName(res.getName());
                 swaggerResource.setLocation(res.getEndpoint());
