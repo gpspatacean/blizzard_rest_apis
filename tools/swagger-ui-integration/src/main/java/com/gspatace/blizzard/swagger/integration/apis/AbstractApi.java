4f5f657be956c5892b50bc695522af169724c55f
George Spatacean <george.spatacean@gmail.com>
2021-04-20 16:34:35 +0300

    Use Spring`s ClassPathResource, as this one handles running the application from command line, as well

--- tools/swagger-ui-integration/src/main/java/com/gspatace/blizzard/swagger/integration/apis/AbstractApi.java
+++ tools/swagger-ui-integration/src/main/java/com/gspatace/blizzard/swagger/integration/apis/AbstractApi.java
@@ -1,20 +1,25 @@
 package com.gspatace.blizzard.swagger.integration.apis;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 
 @Slf4j
 public abstract class AbstractApi {
     protected String getContent(String specificationFile) {
+        log.info("Loading specification file input: {}", specificationFile);
         StringBuilder specification = new StringBuilder();
-        try (Stream<String> lines = Files.lines(Paths.get(ClassLoader.getSystemResource(specificationFile).toURI()))) {
+        Resource resource = new ClassPathResource(specificationFile);
+        try (Stream<String> lines = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)).lines()) {
             lines.forEach(line -> specification.append(line).append(System.lineSeparator()));
-        } catch (URISyntaxException | IOException ex) {
+            log.trace("{} file content: {}", specificationFile, specification.toString());
+        } catch (IOException ex) {
             log.error("Exception occurred while parsing specification file {} :", specificationFile, ex);
         }
         return specification.toString();
